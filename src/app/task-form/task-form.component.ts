cc8475318d677d7e097976b5fef0d09455848ddb
Artikunazo <artik16032011@gmail.com>
2024-04-02 11:16:26 -0600

    [FT] Task form component added

---
+++ src/app/task-form/task-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'task-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-form.component.html',
+  styleUrl: './task-form.component.scss'
+})
+export class TaskFormComponent {
+
+}
