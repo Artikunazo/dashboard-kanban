d7db70cfed5ca92331ae07b1323353eea6a9dfa8
Artikunazo <artik16032011@gmail.com>
2024-04-02 12:40:29 -0600

    [IMP] setup tasks actions setup finished

--- src/app/store/actions/tasks_actions.ts
+++ src/app/store/actions/tasks_actions.ts
@@ -1,6 +1,6 @@
 import {Update} from '@ngrx/entity';
 import {Action} from '@ngrx/store';
-import {TasksModels} from 'src/app/models/tasks_models';
+import {ITask} from '../../models/tasks_modes';
 
 export enum TasksActionType {
 	LOAD_TASKS = '[Task] Load Tasks',
@@ -19,3 +19,93 @@ export enum TasksActionType {
 	DELETE_TASK_SUCCESS = '[Task] Delete Task Success',
 	DELETE_TASK_FAIL = '[Task] Delete Task Fail',
 }
+
+// LOAD
+export class LoadTasks implements Action {
+	readonly type = TasksActionType.LOAD_TASKS;
+
+	constructor(public payload: ITask) {}
+}
+
+export class LoadTasksSuccess implements Action {
+	readonly type = TasksActionType.LOAD_TASKS_SUCCESS;
+
+	constructor(public payload: string) {}
+}
+
+export class LoadTasksFail implements Action {
+	readonly type = TasksActionType.LOAD_TASKS_FAIL;
+
+	constructor(public payload: string) {}
+}
+
+// ADD
+export class AddTask implements Action {
+	readonly type = TasksActionType.ADD_TASK;
+
+	constructor(public payload: string) {}
+}
+
+export class AddTaskSuccess implements Action {
+	readonly type = TasksActionType.ADD_TASK_SUCCESS;
+
+	constructor(public payload: string) {}
+}
+
+export class AddTaskFail implements Action {
+	readonly type = TasksActionType.ADD_TASK_FAIL;
+
+	constructor(public payload: string) {}
+}
+
+// UPDATE
+export class UpdateTask implements Action {
+	readonly type = TasksActionType.LOAD_TASKS;
+
+	constructor(public payload: string) {}
+}
+
+export class UpdateTasksSuccess implements Action {
+	readonly type = TasksActionType.UPDATE_TASK_SUCCESS;
+
+	constructor(public payload: Update<ITask>) {}
+}
+
+export class UpdateTasksFail implements Action {
+	readonly type = TasksActionType.UPDATE_TASK_FAIL;
+
+	constructor(public payload: string) {}
+}
+
+// DELETE
+export class DeleteTask implements Action {
+	readonly type = TasksActionType.DELETE_TASK;
+
+	constructor(public payload: string) {}
+}
+
+export class DeleteTaskSuccess implements Action {
+	readonly type = TasksActionType.DELETE_TASK_SUCCESS;
+
+	constructor(public payload: string) {}
+}
+
+export class DeleteTaskFail implements Action {
+	readonly type = TasksActionType.DELETE_TASK_FAIL;
+
+	constructor(public payload: string) {}
+}
+
+export type TasksActions =
+	| LoadTasks
+	| LoadTasksSuccess
+	| LoadTasksFail
+	| AddTask
+	| AddTaskSuccess
+	| AddTaskFail
+	| UpdateTask
+	| UpdateTasksSuccess
+	| UpdateTasksFail
+	| DeleteTask
+	| DeleteTaskSuccess
+	| DeleteTaskFail;
