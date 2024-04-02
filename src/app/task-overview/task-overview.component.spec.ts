8eb4c86c1646831e1e36cfe592fd85a7a42fa151
Artikunazo <artik16032011@gmail.com>
2024-04-02 11:16:42 -0600

    [FT] Task overview component added

---
+++ src/app/task-overview/task-overview.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TaskOverviewComponent } from './task-overview.component';
+
+describe('TaskOverviewComponent', () => {
+  let component: TaskOverviewComponent;
+  let fixture: ComponentFixture<TaskOverviewComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TaskOverviewComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(TaskOverviewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/task-overview/task-overview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'task-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-overview.component.html',
+  styleUrl: './task-overview.component.scss'
+})
+export class TaskOverviewComponent {
+
+}
