0c28e8667ebf92931a9b4d39003c77df1b9e86cb
Artikunazo <artik16032011@gmail.com>
2024-04-02 11:09:57 -0600

    [FT] Kanban card added

---
+++ src/app/common/kanban-card/kanban-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kanban-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './kanban-card.component.html',
+  styleUrl: './kanban-card.component.scss'
+})
+export class KanbanCardComponent {
+
+}
