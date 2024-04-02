da9583bf8734c8f14ba1e4e0fe0e5909a47571a6
Artikunazo <artik16032011@gmail.com>
2024-04-02 03:02:32 -0600

    [IMP] Sidebar options added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {ToolbarComponent} from './toolbar/toolbar.component';
+import {MatListModule} from '@angular/material/list';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [RouterOutlet, ToolbarComponent],
+	imports: [
+		RouterOutlet,
+		ToolbarComponent,
+		MatListModule,
+		MatSidenavModule,
+		MatIconModule,
+		MatSlideToggleModule,
+	],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
 })
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,7 +3,6 @@ import {CommonModule} from '@angular/common';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {MatIconModule} from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -16,7 +15,6 @@ import {MatTooltipModule} from '@angular/material/tooltip';
 		MatInputModule,
 		MatFormFieldModule,
 		MatButtonModule,
-		MatButtonToggleModule,
 		MatIconModule,
 		MatCardModule,
 		MatToolbarModule,
