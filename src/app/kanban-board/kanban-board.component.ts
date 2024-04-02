ba9b5867584e6221457e7d2aaa09435fae0aa603
Artikunazo <artik16032011@gmail.com>
2024-04-02 11:44:52 -0600

    [FT] Add kanban board component

---
+++ src/app/kanban-board/kanban-board.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kanban-board',
+  standalone: true,
+  imports: [],
+  templateUrl: './kanban-board.component.html',
+  styleUrl: './kanban-board.component.scss'
+})
+export class KanbanBoardComponent {
+
+}
