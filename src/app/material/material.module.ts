235a63150e65fd0bdae931c9993af2fa037c40c3
Artikunazo <artik16032011@gmail.com>
2024-04-02 02:19:13 -0600

    [FT] Add material modules needed

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,26 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @NgModule({
 	declarations: [],
 	imports: [CommonModule],
+	exports: [
+		MatInputModule,
+		MatFormFieldModule,
+		MatButtonModule,
+		MatButtonToggleModule,
+		MatIconModule,
+		MatCardModule,
+		MatToolbarModule,
+		MatTooltipModule,
+	],
 })
 export class MaterialModule {}
