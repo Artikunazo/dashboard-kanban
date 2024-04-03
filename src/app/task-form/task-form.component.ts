45b1ded6b8dad9c7da21a2c5d6b4bb4d199be403
Artikunazo <artik16032011@gmail.com>
2024-04-02 23:20:25 -0600

    [FT] create new task form init structure finished

--- src/app/task-form/task-form.component.ts
+++ src/app/task-form/task-form.component.ts
@@ -1,5 +1,10 @@
 import {Component, inject} from '@angular/core';
-import {ReactiveFormsModule, Validators, FormBuilder} from '@angular/forms';
+import {
+	ReactiveFormsModule,
+	Validators,
+	FormBuilder,
+	FormArray,
+} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
@@ -21,13 +26,28 @@ import {CustomButtonComponent} from '../common/custom-button/custom-button.compo
 export class TaskFormComponent {
 	protected formBuilder = inject(FormBuilder);
 
+	// public subtasks = this.formBuilder.group({
+	// 	title: this.formBuilder.control('', [Validators.required]),
+	// });
+
 	public taskForm = this.formBuilder.group({
 		title: this.formBuilder.control('', [Validators.required]),
 		description: this.formBuilder.control('', [Validators.required]),
-		subtasks: this.formBuilder.control([], [Validators.required]),
+		subtasks: this.formBuilder.array([
+			this.formBuilder.control('', [Validators.required]),
+		]),
 		status: this.formBuilder.control('', [Validators.required]),
 	});
+
 	public statusOptions = ['ToDo', 'Doing', 'Done'];
+
+	get subtasks() {
+		return this.taskForm.get('subtasks') as FormArray;
+	}
+
+	addSubtask() {
+		return this.subtasks.push(this.formBuilder.control(''));
+	}
 
 	createTask() {}
 }
