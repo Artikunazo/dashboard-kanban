b25a563bc730217282aab03a96b9da8acb2f8c50
Artikunazo <artik16032011@gmail.com>
2024-04-03 15:26:52 -0600

    [FT] Add data service to get and save data

--- src/app/api/data.service.ts
+++ src/app/api/data.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {of} from 'rxjs';
 import {ITask} from '../models/tasks_models';
+import * as fromTasksActions from '../store/actions/tasks_actions';
 
 @Injectable({
 	providedIn: 'root',
@@ -9,10 +10,18 @@ export class DataService {
 	constructor() {}
 
 	saveData(data: ITask) {
-		localStorage.setItem('kanban', JSON.stringify(data));
+		const kanbanData = JSON.parse(
+			this.loadDataWithoutObservable() ?? JSON.stringify([]),
+		);
+		const newKanbanData = [...kanbanData, data];
+		localStorage.setItem('kanban', JSON.stringify(newKanbanData));
 	}
 
 	loadData() {
 		return of(localStorage.getItem('kanban'));
+	}
+
+	loadDataWithoutObservable() {
+		return localStorage.getItem('kanban');
 	}
 }
