dc99a7e3db171012b3bce7c940d25874d3cf3d2c
Artikunazo <artik16032011@gmail.com>
2024-04-03 15:26:34 -0600

    [IMP] Import effects to app config file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,16 +3,19 @@ import {provideRouter} from '@angular/router';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {provideEffects} from '@ngrx/effects';
 import {provideState, provideStore} from '@ngrx/store';
+import {provideStoreDevtools} from '@ngrx/store-devtools';
 
 import * as fromTasksReducer from './store/reducers/tasks_reducer';
 import {routes} from './app.routes';
+import {effects} from './store';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideRouter(routes),
 		provideAnimationsAsync(),
-		provideEffects(),
+		provideEffects(effects),
 		provideStore(),
 		provideState({name: 'tasks', reducer: fromTasksReducer.reducer}),
+		provideStoreDevtools(),
 	],
 };
