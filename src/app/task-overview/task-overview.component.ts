9260b3e7d7e0371322cea40a604097c76ad31845
Artikunazo <artik16032011@gmail.com>
2024-04-03 18:30:39 -0600

    [IMP] Set formControl to status dropdown in task overview

--- src/app/task-overview/task-overview.component.ts
+++ src/app/task-overview/task-overview.component.ts
@@ -1,23 +1,38 @@
-import {Component, OnInit, inject, input} from '@angular/core';
+import {Component, OnInit, inject} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {SubtasksOverviewComponent} from '../common/subtasks-overview/subtasks-overview.component';
 import {MatSelectModule} from '@angular/material/select';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
 	selector: 'task-overview',
 	standalone: true,
-	imports: [SubtasksOverviewComponent, MatSelectModule, MatFormFieldModule],
+	imports: [
+		SubtasksOverviewComponent,
+		MatSelectModule,
+		MatFormFieldModule,
+		FormsModule,
+		ReactiveFormsModule,
+	],
 	templateUrl: './task-overview.component.html',
 	styleUrl: './task-overview.component.scss',
 })
-export class TaskOverviewComponent {
+export class TaskOverviewComponent implements OnInit {
 	protected readonly matDialogData = inject(MAT_DIALOG_DATA);
 	protected readonly dialogRef = inject(
 		MatDialogRef,
 	) as MatDialogRef<TaskOverviewComponent>;
 
 	public task = this.matDialogData;
-	public statusSelected = this.task.status;
+	public statusSelected = new FormControl(this.task.status);
 	public statusOptions = ['ToDo', 'Doing', 'Done'];
+
+	ngOnInit(): void {
+		this.statusSelected.valueChanges.subscribe({
+			next: (newValue) => {
+				console.log(newValue);
+			},
+		});
+	}
 }
