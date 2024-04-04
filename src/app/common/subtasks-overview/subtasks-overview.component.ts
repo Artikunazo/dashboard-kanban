7b6b737979ec28315be5b8f528d7508a8690a840
Artikunazo <artik16032011@gmail.com>
2024-04-03 18:10:02 -0600

    [IMP] Show task information in modal

--- src/app/common/subtasks-overview/subtasks-overview.component.ts
+++ src/app/common/subtasks-overview/subtasks-overview.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, input} from '@angular/core';
+import {ISubtasks} from 'src/app/models/tasks_models';
 
 @Component({
-  selector: 'subtasks-overview',
-  standalone: true,
-  imports: [],
-  templateUrl: './subtasks-overview.component.html',
-  styleUrl: './subtasks-overview.component.scss'
+	selector: 'subtasks-overview',
+	standalone: true,
+	imports: [],
+	templateUrl: './subtasks-overview.component.html',
+	styleUrl: './subtasks-overview.component.scss',
 })
 export class SubtasksOverviewComponent {
-
+	public subtask = input<ISubtasks>();
 }
--- src/app/task-overview/task-overview.component.ts
+++ src/app/task-overview/task-overview.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, inject, input} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {SubtasksOverviewComponent} from '../common/subtasks-overview/subtasks-overview.component';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
-  selector: 'task-overview',
-  standalone: true,
-  imports: [],
-  templateUrl: './task-overview.component.html',
-  styleUrl: './task-overview.component.scss'
+	selector: 'task-overview',
+	standalone: true,
+	imports: [SubtasksOverviewComponent, MatSelectModule, MatFormFieldModule],
+	templateUrl: './task-overview.component.html',
+	styleUrl: './task-overview.component.scss',
 })
 export class TaskOverviewComponent {
+	protected readonly matDialogData = inject(MAT_DIALOG_DATA);
+	protected readonly dialogRef = inject(
+		MatDialogRef,
+	) as MatDialogRef<TaskOverviewComponent>;
 
+	public task = this.matDialogData;
+	public statusSelected = this.task.status;
+	public statusOptions = ['ToDo', 'Doing', 'Done'];
 }
