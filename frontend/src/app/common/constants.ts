a3df3193e713403ee15f700ceb8e81d02778b502
Arturo <artik16032011@gmail.com>
2025-10-09 00:27:32 -0600

    update api url

--- frontend/src/app/common/constants.ts
+++ frontend/src/app/common/constants.ts
@@ -1,2 +1,4 @@
-export const URL_BASE = 'http://localhost:8080/';
+import { environment } from "src/environments/environment.prod";
+
+export const URL_BASE = environment.apiUrl ?? 'https://dashboard-kanban.onrender.com/';
 export const CONTEXT_PATH = 'dashboard-kanban/api/';
